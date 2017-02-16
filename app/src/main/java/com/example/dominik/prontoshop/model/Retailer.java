28eb711201e8113dfe39206705cbd8e0e02ab259
Faelivrin <jurasz.do@gmail.com>
2017-02-16 10:36:55 +0100

    Added Customer, Retailer and Transaction Model. (Transaction have convert complex object to json)

---
+++ app/src/main/java/com/example/dominik/prontoshop/model/Retailer.java
@@ -0,0 +1,18 @@
+package com.example.dominik.prontoshop.model;
+
+public class Retailer
+{
+    private long id;
+    private String buisnessName;
+    private String emailAddress;
+    private String phoneNumber;
+    private String streetAddress;
+    private String streetAddress2;
+    private String city;
+    private String state;
+    private String zip;
+    private String industry;
+    private String contactPerson;
+
+    public Retailer(){}
+}
