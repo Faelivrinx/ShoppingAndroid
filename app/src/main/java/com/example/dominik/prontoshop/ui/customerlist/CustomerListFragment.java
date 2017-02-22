e669c04249541b840b59476116d1cffb5029b469
Dominik <jurasz.do@gmail.com>
2017-02-22 16:59:04 +0100

    Implement product and customer list.

--- app/src/main/java/com/example/dominik/prontoshop/ui/customerlist/CustomerListFragment.java
+++ app/src/main/java/com/example/dominik/prontoshop/ui/customerlist/CustomerListFragment.java
@@ -2,19 +2,42 @@ package com.example.dominik.prontoshop.ui.customerlist;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.dominik.prontoshop.R;
+import com.example.dominik.prontoshop.core.listeners.OnCustomerSelectedListener;
+import com.example.dominik.prontoshop.model.Customer;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class CustomerListFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CustomerListFragment extends Fragment implements OnCustomerSelectedListener {
 
     private View mRootView;
+    private CustomerListAdapter mAdapter;
+
+
+    @BindView(R.id.fragment_customer_list_recyclerView)
+    RecyclerView mRecyclerView;
+
+    @BindView(R.id.fragment_customer_list_emptyText)
+    TextView mEmptyText;
+
+    @BindView(R.id.fragment_customer_list_floatActionButton)
+    FloatingActionButton mFab;
+
+
 
     public CustomerListFragment() {
         // Required empty public constructor
@@ -26,7 +49,43 @@ public class CustomerListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mRootView = inflater.inflate(R.layout.fragment_customer_list, container, false);
+
+        ButterKnife.bind(this, mRootView);
+
+        List<Customer> tempCustomers = new ArrayList<>();
+        mAdapter = new CustomerListAdapter(tempCustomers, getActivity(), this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        if(tempCustomers.size() < 1){
+            showEmptyTextMessage();
+        } else {
+            hideEmptyTextMessage();
+        }
+
         return mRootView;
     }
 
+
+    private void hideEmptyTextMessage() {
+        mEmptyText.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void showEmptyTextMessage() {
+        mRecyclerView.setVisibility(View.GONE);
+        mEmptyText.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onSelectCustomer(Customer customer) {
+
+
+    }
+
+    @Override
+    public void onLongClickedCustomer(Customer customer) {
+
+    }
 }
--- app/src/main/java/com/example/dominik/prontoshop/ui/productlist/ProductListFragment.java
+++ app/src/main/java/com/example/dominik/prontoshop/ui/productlist/ProductListFragment.java
@@ -2,16 +2,42 @@ package com.example.dominik.prontoshop.ui.productlist;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.dominik.prontoshop.R;
+import com.example.dominik.prontoshop.core.listeners.OnProductSelectedListener;
+import com.example.dominik.prontoshop.model.Product;
 
-public class ProductListFragment extends Fragment {
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ProductListFragment extends Fragment implements OnProductSelectedListener {
 
     private View mRootView;
+    private ProductListAdapter mAdapter;
+
+
+    @BindView(R.id.fragment_product_list_recyclerView)
+    RecyclerView mRecyclerView;
+
+    @BindView(R.id.fragment_product_list_emptyText)
+    TextView mEmptyText;
+
+    @BindView(R.id.fragment_product_list_floatActionButton)
+    FloatingActionButton mFab;
+
 
     public ProductListFragment() {
         // Required empty public constructor
@@ -22,9 +48,42 @@ public class ProductListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
         mRootView = inflater.inflate(R.layout.fragment_product_list, container, false);
+        ButterKnife.bind(this, mRootView);
+
+        List<Product> tempProducts = new ArrayList<>();
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+
+        mAdapter = new ProductListAdapter(tempProducts, getActivity(), this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        if(tempProducts.size() < 1){
+            showEmptyTextMessage();
+        } else {
+            hideEmptyTextMessage();
+        }
+
+
         return mRootView;
     }
 
+    private void hideEmptyTextMessage() {
+        mEmptyText.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void showEmptyTextMessage() {
+        mRecyclerView.setVisibility(View.GONE);
+        mEmptyText.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onSelectProduct(Product selectedProduct) {
+
+    }
+
+    @Override
+    public void onLongClickProduct(Product clickedProduct) {
+
+    }
 }
